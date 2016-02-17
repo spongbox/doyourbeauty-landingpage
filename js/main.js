ae8782c85be49f4d5dfa388a5ba628693359d3a7
NylsSs <nilsredoulez@gmail.com>
2016-02-17 16:35:13 +0100

    ajout analytics + feedback form

--- js/main.js
+++ js/main.js
@@ -13,7 +13,15 @@ form.querySelector("button").addEventListener('click', function () {
       if (error != null) {
         console.log('error');
       } else {
-        console.log('success');
+        var textElmnt = document.getElementById('formsub-title');
+        textElmnt.parentNode.removeChild(textElmnt);
+        var sp1 = document.createElement("span");
+        sp1.setAttribute("id", "confirm_subs");
+        var sp1_content = document.createTextNode("Merci ! Votre adresse à bien été prise en compte.");
+        sp1.appendChild(sp1_content);
+        var sp2 = document.getElementById("email-registration");
+        var parentDiv = sp2.parentNode;
+        parentDiv.replaceChild(sp1, sp2);
       }
     });
   }
